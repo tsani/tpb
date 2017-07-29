1d46fcfcf13af18a8436469fc2f2639d3dd76b52
Jacob Errington <git@mail.jerrington.me>
2017-07-29 19:07:45 +0000

    pb-notify: implement clipboard integration
    pb-notify now maintains an internal buffer to represent the pushbullet
    clipboard. When Clipboard events come down the stream, this buffer is
    updated.
    An HTTP server is added to pb-notify.
    A single endpoint is implemented, allowed a user to retrieve the
    contents of the buffer and to broadcast a new clipboard synchronization
    message to PushBullet. Other devices configured to use Universal
    Copy/Paste will receive this text in their clipboards.
    
    Note that no interaction with the system clipboard is currently
    provided.

--- src/pb-notify/Main.hs
+++ src/pb-notify/Main.hs
@@ -3,6 +3,7 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeOperators #-}
 
 module Main where
 
@@ -11,29 +12,38 @@ import Network.Pushbullet.Client
 import Network.Pushbullet.Misc
 import Network.Pushbullet.Types
 
-import Control.Concurrent ( forkIO, threadDelay )
+import Control.Concurrent ( threadDelay )
 import Control.Concurrent.Async
 import Control.Concurrent.Chan
+import Control.Concurrent.MVar
 import Control.Monad ( forever, forM_ )
-import Data.Aeson ( eitherDecode' )
+import Control.Monad.IO.Class
+import Data.Aeson ( eitherDecode', encode )
 import qualified Data.ByteString.Char8 as C8
 import qualified Data.ByteString.Lazy as LBS
 import Data.IORef
 import Data.List ( sortBy )
+import qualified Data.Map.Strict as M
 import Data.Maybe ( listToMaybe )
 import Data.Monoid ( (<>) )
 import Data.Ord ( comparing )
 import qualified Data.Text as T
+import qualified Data.Text.IO as T
 import Data.Time.Format ( defaultTimeLocale, formatTime )
 import Data.Time.LocalTime ( getTimeZone, utcToLocalTime )
 import qualified Libnotify as Noti
 import Network.HTTP.Client ( newManager )
 import Network.HTTP.Client.TLS ( tlsManagerSettings )
+import Network.Wai ( Application )
+import Network.Wai.Handler.Warp ( run )
 import qualified Network.WebSockets as WS
+import Servant
 import Servant.Client
+import System.IO ( hPutStrLn, stderr )
 import System.Environment ( getEnv )
 import System.Exit ( exitFailure )
 import System.Timeout ( timeout )
+import Text.Read ( readMaybe )
 import Wuss ( runSecureClient )
 
 appName :: String
@@ -42,26 +52,215 @@ appName = "pb-notify"
 timeoutDelay :: Int
 timeoutDelay = 5000000 -- five seconds
 
+-- | A map from text (identifiers in the PushBullet API) to libnotify
+-- notification.
+newtype NotifyMap
+  = NotifyMap
+    { unNotifyMap :: M.Map T.Text Noti.Notification }
+
+newtype NotifyMapVar
+  = NotifyMapVar
+    { unNotifyMapVar :: MVar NotifyMap }
+
+data ClipDataVar
+  = ClipDataVar
+    { clipDataLock :: MVar Bool
+      -- ^ The lock is used to guard access to the IORef and to indicate
+      -- whether the IORef contains meaningful data
+    , clipData :: IORef T.Text
+    }
+
+newClipDataVar :: IO ClipDataVar
+newClipDataVar = do
+  v <- newMVar False
+  r <- newIORef ""
+  pure ClipDataVar { clipDataLock = v, clipData = r }
+
+-- | Reads the clip data; if it's empty then Nothing is returned.
+readClipVar :: ClipDataVar -> IO (Maybe T.Text)
+readClipVar (ClipDataVar lock r) = withMVar lock $ \case
+  True -> Just <$> readIORef r
+  False -> pure Nothing
+
+newtype ClipChan
+  = ClipChan
+    { unClipChan :: Chan () }
+
+newClipChan :: IO ClipChan
+newClipChan = ClipChan <$> newChan
+
+putClipChan :: ClipChan -> IO ()
+putClipChan (ClipChan c) = writeChan c ()
+
+getClipChan :: ClipChan -> IO ()
+getClipChan (ClipChan c) = readChan c
+
+newtype HttpChan
+  = HttpChan
+    { unHttpChan :: Chan HttpReq }
+
+-- | The types of things we can ask the HTTP client thread to do.
+data HttpReq
+  -- | Send the given text to the stream.
+  = SendClip T.Text
+  -- | Check for new pushes and convert to notifications.
+  | CheckPushes
+
+newHttpChan :: IO HttpChan
+newHttpChan = HttpChan <$> newChan
+
+checkPushes :: HttpChan -> IO ()
+checkPushes (HttpChan c) = writeChan c CheckPushes
+
+sendClip :: HttpChan -> T.Text -> IO ()
+sendClip (HttpChan c) t = writeChan c (SendClip t)
+
+getHttpChan :: HttpChan -> IO HttpReq
+getHttpChan (HttpChan c) = readChan c
+
+withNotifyMap :: NotifyMapVar -> (NotifyMap -> IO (NotifyMap, a)) -> IO a
+withNotifyMap (NotifyMapVar v) m = do modifyMVar v m
+
+type PbNotifyApi =
+  "clip" :> (
+    Get '[PlainText] T.Text
+  :<|>
+    ReqBody '[PlainText] T.Text :> PostAccepted '[PlainText] NoContent
+  )
+
+pbNotifyApi :: Proxy PbNotifyApi
+pbNotifyApi = Proxy
+
+addNoti
+  :: Noti.Mod Noti.Notification
+  -> T.Text
+  -> NotifyMap
+  -> IO NotifyMap
+addNoti n t (NotifyMap m)= do
+  noti <- Noti.display n
+  pure $ NotifyMap (M.insert t noti m)
+
+deleteNoti :: T.Text -> NotifyMap -> IO NotifyMap
+deleteNoti t (NotifyMap m) = do
+  case M.lookup t m of
+    Nothing -> pure (NotifyMap m)
+    Just n -> do
+      Noti.close n
+      pure $ NotifyMap (M.delete t m)
+
+webapp :: ClipDataVar -> HttpChan -> Application
+webapp clipvar httpChan = serve pbNotifyApi (server clipvar httpChan)
+
+server :: ClipDataVar -> HttpChan -> Server PbNotifyApi
+server (ClipDataVar lock var) httpChan = getClip :<|> postClip where
+  getClip :: Handler T.Text
+  getClip = do
+    liftIO (takeMVar lock) >>= \case
+      False -> do
+        liftIO (putMVar lock False)
+        throwError err404 { errBody = "no clipboard data" }
+      True -> do
+        t <- liftIO (readIORef var)
+        liftIO (putMVar lock True)
+        pure t
+
+  postClip :: T.Text -> Handler NoContent
+  postClip t = do
+    liftIO $ modifyMVar_ lock $ \b -> do
+      -- writeIORef var t
+      sendClip httpChan t
+      -- pure True
+      pure b
+    pure NoContent
+
+streamUrl :: String
+streamUrl = "stream.pushbullet.com"
+
+apiUrl :: String
+apiUrl = "api.pushbullet.com"
+
+die :: String -> IO a
+die s = hPutStrLn stderr s *> exitFailure
+
 main :: IO ()
 main = do
   token <- getEnv "PUSHBULLET_KEY"
-  chan <- newChan
-  _ <- forkIO (http chan (PushbulletKey (T.pack token)))
+  deviceIdS <- getEnv "PBNOTIFY_DEVICE"
+  listenPort <- do
+    p <- getEnv "PBNOTIFY_PORT"
+    maybe (die "PBNOTIFY_PORT must be an integer") pure $ readMaybe p
+
+  let key = PushbulletKey (T.pack token)
+
+  -- strategy for making clipboard ephemerals
+  makeClipEphemeral <- do
+    uid <- _userId <$> getUser key
+    let did = DeviceId (T.pack deviceIdS)
+
+    pure $ \x -> PushEphemeral (Just allEphemeralTargets) Clipboard
+      { _ephClipBody = x
+      , _ephClipSourceUser = uid
+      , _ephClipSourceDevice = did
+      }
+
+  httpChan <- newHttpChan
+  clipChan <- newClipChan
+  clipvar <- newClipDataVar
+  clipflag <- newIORef True
+
+  notiVar <- NotifyMapVar <$> newMVar (NotifyMap M.empty)
+
+  -- run the asynchronous http thread
+  -- This thread gets poked over the chan whenever a Tickle event is received
+  -- over the websocket. This causes the list of pushes to get updated, and
+  -- transformed into notifications.
+  -- HTTP requests sent by this thread are retried indefinitely until they
+  -- succeed.
+  _ <- async (http notiVar httpChan clipvar makeClipEphemeral key)
+
+  -- run the http server thread
+  -- This thread runs a dead simple API to access the pushbullet clipboard
+  -- POST requests to /clip will set the internal variable holding the PB
+  -- clipboard and send the clipboard ephemeral over the websocket so other
+  -- devices will have their clipboards set.
+  -- GET requests will retrieve the value of the internal variable holding the
+  -- PB clipboard. If no clipboard data has been received yet via an ephemeral
+  -- on the websocket, this produces a 404.
+  -- Note that the local clipboard is unaffected by these actions!
+  _ <- async (run listenPort (webapp clipvar httpChan))
+
+  let wsclient = runSecureClient streamUrl 443 ("/websocket/" <> token)
+
   forever $ do
     a <- async $ do
-      runSecureClient
-        "stream.pushbullet.com"
-        443
-        ("/websocket/" <> token)
-        (ws chan)
+      wsclient (ws httpChan clipvar clipChan)
+
     waitCatch a >>= \case
       Left err -> print err
       Right () -> pure ()
+
     threadDelay timeoutDelay
     putStrLn "restarting websocket connection..."
 
-http :: Chan () -> PushbulletKey -> IO ()
-http chan key = do
+getUser :: PushbulletKey -> IO User
+getUser key = do
+  let auth = pushbulletAuth key
+
+  manager <- newManager tlsManagerSettings
+  let url = BaseUrl Https "api.pushbullet.com" 443 ""
+  let env = ClientEnv manager url
+  let runClient = {- debug -} retryingDelay timeoutDelay . flip runClientM env
+
+  runClient (getMe auth)
+
+http
+  :: NotifyMapVar
+  -> HttpChan
+  -> ClipDataVar
+  -> (T.Text -> Ephemeral)
+  -> PushbulletKey
+  -> IO ()
+http notiVar httpChan clipvar mke key = do
   let auth = pushbulletAuth key
 
   manager <- newManager tlsManagerSettings
@@ -80,51 +279,81 @@ http chan key = do
 
   forever $ do
     -- block until a new request
-    _ <- readChan chan
-    lastPushTime <- readIORef lastPushTimeVar
-    let f = fmap (fmap unExistingPushes)
-    let getPushes' time active n = f . runClient . getPushes auth time active n
-    let getPushes'' = getPushes' (Just lastPushTime) (Just True) Nothing
-    start <- getPushes'' Nothing
-    let next = getPushes'' . Just
-    pushes <- sortBy (comparing pushModified)
-      <$> getPaginatedLimit All start next
-    forM_ pushes $ \Push{ pushData, pushModified } -> do
-      writeIORef lastPushTimeVar pushModified
+    r <- getHttpChan httpChan
 
-      Noti.display_ . mconcat $ case pushData of
-        NotePush{..} -> let title = maybe "[untitled]" T.unpack pushTitle in
-          [ Noti.summary ("Note: " ++ title)
-          , Noti.body (T.unpack pushBody)
-          , Noti.appName appName
-          ]
-        LinkPush{..} -> let title = maybe "[untitled]" T.unpack pushTitle in
-          [ Noti.summary ("Link: " ++ title)
-          , Noti.body (T.unpack pushBody)
-          , Noti.appName appName
-          ]
-        FilePush{..} ->
-          [ Noti.summary ("File: " ++ T.unpack pushFileName)
-          , Noti.body (T.unpack (unUrl pushFileUrl))
-          , Noti.appName appName
-          ]
+    case r of
+      SendClip t -> runClient (createEphemeral auth (mke t)) *> pure ()
 
-ws :: Chan () -> WS.ClientApp ()
-ws chan connection = do
-  rawm <- timeout 35000000 (WS.receiveData connection)
-  case rawm of
-    Nothing -> putStrLn "websocket receive timed out"
-    Just raw -> handle chan raw *> ws chan connection
+      CheckPushes -> do
+        lastPushTime <- readIORef lastPushTimeVar
+        let f = fmap (fmap unExistingPushes)
+        getPushes' <- pure $ \time active n ->
+          f . runClient . getPushes auth time active n
+        let getPushes'' = getPushes' (Just lastPushTime) (Just True) Nothing
+        start <- getPushes'' Nothing
+        let next = getPushes'' . Just
+        pushes <- sortBy (comparing pushModified)
+          <$> getPaginatedLimit All start next
 
-handle :: Chan () -> C8.ByteString -> IO ()
-handle chan raw = do
+        withNotifyMap notiVar $ \notiMap -> do
+          v <- newIORef notiMap
+
+          forM_ pushes $ \Push{..} -> do
+            let (PushId pid) = pushId
+            writeIORef lastPushTimeVar pushModified
+
+            m <- readIORef v
+            let note = preparePushNotification pushData
+            let g = if pushActive then addNoti note else deleteNoti
+            writeIORef v =<< g pid m
+
+          (,) <$> readIORef v <*> pure ()
+
+preparePushNotification :: PushData 'Existing -> Noti.Mod Noti.Notification
+preparePushNotification pushData = mconcat $ case pushData of
+  NotePush{..} ->
+    [ Noti.summary ("Note: " ++ maybe "[untitled]" T.unpack pushTitle)
+    , Noti.body (T.unpack pushBody)
+    , Noti.appName appName
+    ]
+  LinkPush{..} ->
+    [ Noti.summary ("Link: " ++ maybe "[untitled]" T.unpack pushTitle)
+    , Noti.body (T.unpack pushBody)
+    , Noti.appName appName
+    ]
+  FilePush{..} ->
+    [ Noti.summary ("File: " ++ T.unpack pushFileName)
+    , Noti.body (T.unpack (unUrl pushFileUrl))
+    , Noti.appName appName
+    ]
+
+ws
+  :: HttpChan
+  -- ^ The channel used to wake up the HTTP thread to check for new pushes
+  -> ClipDataVar
+  -- ^ The variable to store our clipboard buffer in
+  -> ClipChan
+  -- ^ The channel we wait on for requests to send the clipboard to pushbullet
+  -> WS.ClientApp ()
+ws httpChan clipvar clipChan connection = recv where
+  recv :: IO ()
+  recv = do
+    -- this timeout is set to 35s. Pushbullet is supposed to send us a
+    -- heartbeat every 30s.
+    rawm <- timeout 35000000 (WS.receiveData connection)
+    case rawm of
+      Nothing -> putStrLn "websocket receive timed out"
+      Just raw -> handle httpChan clipvar raw *> recv
+
+handle :: HttpChan -> ClipDataVar -> C8.ByteString -> IO ()
+handle httpChan (ClipDataVar lock clip) raw = do
   let message = eitherDecode' (LBS.fromStrict raw)
   case message of
     Right x -> case x :: Ephemeral of
       Tickle t -> case t of
-        PushType -> writeChan chan ()
+        PushType -> checkPushes httpChan
         OtherType t' -> putStrLn $ "got other tickle: " ++ T.unpack t'
-      PushEphemeral p -> case p of
+      PushEphemeral targets p -> case p of
         SmsChanged{..} ->
           forM_ _ephNotifications $ \Notification{..} -> do
             t <- niceTime _notifTime
@@ -133,6 +362,10 @@ handle chan raw = do
               , Noti.body (T.unpack (_notifBody <> "\n") <> t)
               , Noti.appName appName
               ]
+        Clipboard{..} ->
+          modifyMVar_ lock $ const $ do
+            writeIORef clip _ephClipBody
+            pure True
         _ -> pure ()
       _ -> pure ()
     Left _ -> pure ()
